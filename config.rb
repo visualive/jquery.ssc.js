f8d97abc2c3a81e5264222f3ba89095d4dbe0f1b
KUCKLU <kuck1u@visualive.jp>
2016-05-26 14:07:32 +0900

    no message

--- config.rb
+++ config.rb
@@ -3,21 +3,21 @@ Encoding.default_external = "utf-8"
 add_import_path "libs/foundation/bower_components/foundation/scss"
 
 # Set this to the root of your project when deployed:
-http_path = "./"
+http_path = "jquery.ssc.js/"
 css_dir = "assets/css"
 sass_dir = "assets/scss"
-images_dir = "assets/imgs"
-generated_images_dir = "assets/img/sprite"
-http_images_path = "../img"
-http_generated_images_dir = "../img/sprite"
+images_dir = "assets/img"
 javascripts_dir = "assets/js"
+generated_images_dir = "assets/img/sprite"
+http_images_path = "jquery.ssc.js/assets/img"
+http_generated_images_dir = "jquery.ssc.js/assets/sprite"
 
 # You can select your preferred output style here (can be overridden via the command line):
 # output_style = :expanded or :nested or :compact or :compressed
 output_style = :expanded
 
 # To enable relative paths to assets via compass helper functions. Uncomment:
-# relative_assets = true
+relative_assets = true
 
 # To disable debugging comments that display the original location of your selectors. Uncomment:
 line_comments = false
